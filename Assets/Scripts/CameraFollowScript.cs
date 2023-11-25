6d3c8f3f3b8de5625490b4557eb9e41a4ec9618a
Madderry <maddewac@gmail.com>
2023-11-25 17:00:37 +0100

    camera smooth

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -1,56 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollowScript : MonoBehaviour
 {
+
+
     public GameObject Camera;       // drag in the main camera here
 
-    public float HorizontalThreshold;
-    public float VerticalThreshold;
+    public float smoothSpeed = 0.125f;
+    public Vector3 locationOffset;
 
-    public float Smoothness;    //  low value gives a choppy camera, but better performance(?) High value, and the camera might not catch up! (you can fix that?)
-
-    Vector2 moveVector;
-
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        Camera.transform.position =  new Vector3(transform.position.x, transform.position.y, -10f);
-    }
+        Vector3 smoothedPosition = Vector3.Lerp(Camera.transform.position, transform.position + locationOffset, smoothSpeed);
+        Camera.transform.position = smoothedPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if
-            (transform.position.x > Camera.transform.position.x + HorizontalThreshold)
-        {
-            moveVector.x = 1;
-        } else if
-            (transform.position.x < Camera.transform.position.x - HorizontalThreshold)
-        {
-            moveVector.x = -1;
-        }
-        else
-        {
-            moveVector.x = 0;
-        }
-
-        if
-    (transform.position.y > Camera.transform.position.y + VerticalThreshold)
-        {
-            moveVector.y = 1;
-        }
-        else if
-            (transform.position.y < Camera.transform.position.y - VerticalThreshold)
-        {
-            moveVector.y = -1;
-        }
-        else
-        {
-            moveVector.y = 0;
-        }
-
-        Camera.transform.Translate(moveVector/Smoothness);
     }
 }
